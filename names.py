7ca1fd001a05749ce38c6a87dba3b335cf378018
Agus Silva Sansó <silvasansoagustin@gmail.com>
2026-03-26 23:06:01 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,11 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+
+    nombre=  input("ingresar nombre: ")
+    apellido= input("ingresar apellido:")
+    nombre_completo= (nombre+ " " +apellido)
+    print(f"{nombre_completo.lower()}")
+    print(f"{nombre_completo.title()}")
+    print(f"{nombre_completo.upper()}")
+    print(f"\t{nombre_completo.lower()}")
