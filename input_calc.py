0264e71c22d2c1fc462938958602c2367fc01867
Agus Silva Sansó <silvasansoagustin@gmail.com>
2026-03-26 23:05:32 -0300

    Update input_calc.py

--- input_calc.py
+++ input_calc.py
@@ -1,5 +1,15 @@
 def rectangle():
-    """Lee base y altura de un rectángulo, calcula e imprime
+     """Lee base y altura de un rectángulo, calcula e imprime
     el área y el perímetro.
     """
-    pass
+    
+    base= int(input(""))
+    altura= int(input(""))
+
+    area= base * altura
+    perimetro= base + base + altura + altura
+    print(f"Base: {base}")
+    print(f"Altura: {altura}")
+    print(f"Area: {area}")
+    print(f"Perimetro: {perimetro}")
+
