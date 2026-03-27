680f1009a2152980ac1a2836f6d2472b248a4d26
Agus Silva Sansó <silvasansoagustin@gmail.com>
2026-03-26 23:07:22 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -3,3 +3,7 @@ def slice_simple():
     usando slicing y lower().
     """
     texto = "Awesome"
+    texto_minusculas = texto.lower()
+    print(texto_minusculas[:3])
+    print(texto[2:5])
+    print(texto_minusculas)
